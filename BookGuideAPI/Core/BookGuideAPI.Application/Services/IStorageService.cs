db79b8ca2f3e25e749c02b6c4d87351545007565
Mehmet Akif Albayrak <akifalbayrakoffical@gmail.com>
2025-05-12 01:05:36 +0300

    FeaT:StorageService Added!

---
+++ BookGuideAPI/Core/BookGuideAPI.Application/Services/IStorageService.cs
@@ -0,0 +1,16 @@
+﻿using BookGuideAPI.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookGuideAPI.Application.Services
+{
+    public interface IStorageService
+    {
+        Task<UploadResult> UploadAsync(Upload upload);
+        Task<byte[]> GetAsync(string fileName);
+        Task<bool> DeleteAsync(string fileName);
+    }
+}
