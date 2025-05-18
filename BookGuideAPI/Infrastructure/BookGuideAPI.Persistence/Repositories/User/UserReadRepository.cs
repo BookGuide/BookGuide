1a1f26670ad93bd11ec2f6871d1aa30a7b5b72c8
Mehmet Akif Albayrak <akifalbayrakoffical@gmail.com>
2025-05-18 11:29:59 +0300

    Feat: UserReadRepository Updated

--- BookGuideAPI/Infrastructure/BookGuideAPI.Persistence/Repositories/User/UserReadRepository.cs
+++ BookGuideAPI/Infrastructure/BookGuideAPI.Persistence/Repositories/User/UserReadRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BookGuideAPI.Application.Repositories;
 using BookGuideAPI.Domain.Entities;
 using BookGuideAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,17 @@ namespace BookGuideAPI.Persistence.Repositories
 {
     public class UserReadRepository : ReadRepository<User>, IUserReadRepository
     {
+        private readonly BookGuideDbContext _context;
         public UserReadRepository(BookGuideDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public DbSet<User> Table => _context.Set<User>();
+
+        public async Task<User> CheckLoginCredentialsAsync(string username, string hashedPassword)
+        {
+            return await Table.FirstOrDefaultAsync(u => u.Username == username && u.HashedPassword == hashedPassword);
         }
     }
 }
